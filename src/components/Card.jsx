5a11f741a3cb5ac7e7d83ee0e189c440cc463d17
Ovi <mofachhalhossain101@gmail.com>
2024-03-04 23:53:22 +0600

    conditional rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ import { Box } from "@chakra-ui/react";
 
 // eslint-disable-next-line react/prop-types
 function Avatar({ ...props }) {
+  // eslint-disable-next-line react/prop-types
   const { style, alt } = props;
   return (
     <>
       <img
         src="https://react.dev/images/home/conf2021/cover.svg"
+        // eslint-disable-next-line react/prop-types
         alt={alt.alt}
         style={style}
       />
@@ -15,6 +17,7 @@ function Avatar({ ...props }) {
 }
 
 function Card({ ...props }) {
+  // eslint-disable-next-line react/prop-types
   const { style, alt } = props;
   return (
     <>
